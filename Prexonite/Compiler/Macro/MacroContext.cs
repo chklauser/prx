a2dbf5a1d25d98def128be6f41914e3c7ecc3b2f
Christian Klauser <sealedsun@gmail.com>
2011-04-09 13:26:49 +0200

    Extract MacroSession to cover all MacroContexts in one expansion session.

--- Prexonite/Compiler/Macro/MacroContext.cs
+++ Prexonite/Compiler/Macro/MacroContext.cs
@@ -6,94 +6,108 @@ using Prexonite.Types;
 namespace Prexonite.Compiler.Macro
 {
     /// <summary>
-    /// 
+    /// Provides macros with access to Prexonite and compiler internals.
     /// </summary>
     public class MacroContext
     {
-        private readonly ReadOnlyDictionaryView<string, SymbolEntry> _globalSymbols;
+        #region Representation
+        
         private readonly AstMacroInvocation _invocation;
-        private readonly ReadOnlyDictionaryView<string, SymbolEntry> _localSymbols;
+        
         private readonly CompilerTarget _target;
-        private LoaderOptions _options;
         private readonly bool _isJustEffect;
-        private readonly MacroContext _parentContext;
+        private readonly MacroSession _session;
 
-        public MacroContext(CompilerTarget target, AstMacroInvocation invocation, bool isJustEffect)
+        #endregion
+
+        /// <summary>
+        /// Creates a new macro context in the specified macro session.
+        /// </summary>
+        /// <param name="session">The macro expansion session.</param>
+        /// <param name="invocation">The node that is being expanded.</param>
+        /// <param name="isJustEffect">Whether the nodes return value will be discarded by the surrounding program.</param>
+        public MacroContext(MacroSession session, AstMacroInvocation invocation, bool isJustEffect)
         {
-            if (target == null)
-                throw new ArgumentNullException("target");
+            if (session == null)
+                throw new ArgumentNullException("session");
             if (invocation == null)
                 throw new ArgumentNullException("invocation");
-            _target = target;
+            _target = session.Target;
             _isJustEffect = isJustEffect;
             _invocation = invocation;
-            _localSymbols = new ReadOnlyDictionaryView<string, SymbolEntry>(_target.Symbols);
-            _globalSymbols = new ReadOnlyDictionaryView<string, SymbolEntry>(_target.Loader.Symbols);
-            _parentContext = null;
-        }
 
-        private MacroContext(MacroContext parentContext, AstMacroInvocation invocation, bool isJustEffect)
-        {
-            if (parentContext == null)
-                throw new ArgumentNullException("parentContext");
-            if (invocation == null)
-                throw new ArgumentNullException("invocation");
-            _target = parentContext._target;
-            _localSymbols = parentContext._localSymbols;
-            _globalSymbols = parentContext._globalSymbols;
-            _parentContext = parentContext;
+            _session = null;
         }
 
         #region Accessors 
 
+        /// <summary>
+        /// The node that is being expanded.
+        /// </summary>
         public AstMacroInvocation Invocation
         {
             get { return _invocation; }
         }
 
+        /// <summary>
+        /// Determines whether the macro is used as a get or as a set call.
+        /// </summary>
         public PCall Call
         {
             get { return Invocation.Call; }
         }
 
+        /// <summary>
+        /// Indicates whether the macros return value should produce a return value.
+        /// </summary>
         public bool IsJustEffect
         {
             get { return _isJustEffect; }
         }
 
+        /// <summary>
+        /// Provides access to a copy of the loader options currently in effect.
+        /// </summary>
         public LoaderOptions LoaderOptions
         {
-            get
-            {
-                if (_options == null)
-                {
-                    _options = new LoaderOptions(_target.Loader.ParentEngine, _target.Loader.ParentApplication);
-                    _options.InheritFrom(_target.Loader.Options);
-                }
-                return _target.Loader.Options;
-            }
+            get { return _session.LoaderOptions; }
         }
 
+        /// <summary>
+        /// Returns the loop block (reacting to break, continue) that is currently active. Can be null.
+        /// </summary>
         public ILoopBlock CurrentLoopBlock
         {
-            get { return _target.CurrentLoopBlock; }
+            get { return _session.CurrentLoopBlock; }
         }
 
+        /// <summary>
+        /// Provides read-only access to the local symbol table.
+        /// </summary>
         public ReadOnlyDictionaryView<string, SymbolEntry> LocalSymbols
         {
-            get { return _localSymbols; }
+            get { return _session.LocalSymbols; }
         }
 
+        /// <summary>
+        /// Provides read-only access to the global symbol table.
+        /// </summary>
         public ReadOnlyDictionaryView<string, SymbolEntry> GlobalSymbols
         {
-            get { return _globalSymbols; }
+            get { return _session.GlobalSymbols; }
         }
 
+        /// <summary>
+        /// A reference to the application being compiled.
+        /// </summary>
         public Application Application
         {
             get { return _target.Loader.ParentApplication; }
         }
 
+        /// <summary>
+        /// A reference to the function being compiled.
+        /// </summary>
         public PFunction Function
         {
             get { return _target.Function; }
@@ -103,21 +117,44 @@ namespace Prexonite.Compiler.Macro
 
         #region Compiler interaction
 
+        /// <summary>
+        /// Allocates a temporary variable for this macro expansion session.
+        /// </summary>
+        /// <returns>The (physical) id of a free temporary variable.</returns>
+        /// <remarks>If a temporary variable is not freed during a macro expansion session, 
+        /// it will no longer be considered a temporary variable and cannot be freed in 
+        /// subsequent expansions</remarks>
         public string AllocateTemporaryVariable()
         {
-            throw new NotImplementedException();
+            return _session.AllocateTemporaryVariable();
         }
 
+        /// <summary>
+        /// Marks the temporary variable for freeing at the end of this expansion session.
+        /// </summary>
+        /// <param name="temporaryVariable">The temporary variable to be freed.</param>
         public void FreeTemporaryVariable(string temporaryVariable)
         {
-            throw new NotImplementedException();
+            _session.FreeTemporaryVariable(temporaryVariable);
         }
 
-        public void ReportMessage(ParseMessage message)
+        /// <summary>
+        /// Reports a compiler message (error, warning, info).
+        /// </summary>
+        /// <param name="severity">The message severity (error, warning, info)</param>
+        /// <param name="message">The actual message (human-readable)</param>
+        /// <param name="position">The location in the code associated with the message.</param>
+        /// <remarks>Issuing an error message does not automatically abort execution of the macro.</remarks>
+        public void ReportMessage(ParseMessageSeverity severity, string message, ISourcePosition position = null)
         {
-            _target.Loader.ReportMessage(message);
+            position = position ?? _invocation;
+            _target.Loader.ReportMessage(new ParseMessage(severity,message,position));
         }
 
+        /// <summary>
+        /// Requires that a variable from an outer function scope is shared with the function currently compiled.
+        /// </summary>
+        /// <param name="variable">The physical name of the variable to request.</param>
         public void RequireOuterVariable(string variable)
         {
             _target.RequireOuterVariable(variable);
---
+++ Prexonite/Compiler/Macro/MacroSession.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using Prexonite.Helper;
+
+namespace Prexonite.Compiler.Macro
+{
+    /// <summary>
+    /// Provides information associated with a macro expansion session.
+    /// </summary>
+    public class MacroSession
+    {
+        private readonly CompilerTarget _target;
+        private LoaderOptions _options;
+        private readonly ReadOnlyDictionaryView<string, SymbolEntry> _globalSymbols;
+        private readonly ReadOnlyDictionaryView<string, SymbolEntry> _localSymbols;
+
+        /// <summary>
+        /// Creates a new macro expansion session for the specified compiler target.
+        /// </summary>
+        /// <param name="target">The target to expand macros in.</param>
+        public MacroSession(CompilerTarget target)
+        {
+            if (target == null)
+                throw new ArgumentNullException("target");
+            _target = target;
+            _localSymbols = new ReadOnlyDictionaryView<string, SymbolEntry>(_target.Symbols);
+            _globalSymbols = new ReadOnlyDictionaryView<string, SymbolEntry>(_target.Loader.Symbols);
+        }
+
+        /// <summary>
+        /// Provides read-only access to the local symbol table.
+        /// </summary>
+        public ReadOnlyDictionaryView<string, SymbolEntry> LocalSymbols
+        {
+            get { return _localSymbols; }
+        }
+
+        /// <summary>
+        /// Provides read-only access to the global symbol table.
+        /// </summary>
+        public ReadOnlyDictionaryView<string, SymbolEntry> GlobalSymbols
+        {
+            get { return _globalSymbols; }
+        }
+
+        /// <summary>
+        /// The target the macro expansion session covers.
+        /// </summary>
+        public CompilerTarget Target
+        {
+            [DebuggerStepThrough]
+            get { return _target; }
+        }
+
+        /// <summary>
+        /// A copy of the loader options in effect during this macro expansion.
+        /// </summary>
+        public LoaderOptions LoaderOptions
+        {
+            get
+            {
+                if (_options == null)
+                {
+                    _options = new LoaderOptions(_target.Loader.ParentEngine, _target.Loader.ParentApplication);
+                    _options.InheritFrom(_target.Loader.Options);
+                }
+                return _target.Loader.Options;
+            }
+        }
+
+        public ILoopBlock CurrentLoopBlock
+        {
+            get { return _target.CurrentLoopBlock; }
+        }
+
+        /// <summary>
+        /// Allocates a temporary variable for this macro expansion session.
+        /// </summary>
+        /// <returns>The (physical) id of a free temporary variable.</returns>
+        /// <remarks>If a temporary variable is not freed during a macro expansion session, 
+        /// it will no longer be considered a temporary variable and cannot be freed in 
+        /// subsequent expansions</remarks>
+        public string AllocateTemporaryVariable()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Marks the temporary variable for freeing at the end of this expansion session.
+        /// </summary>
+        /// <param name="temporaryVariable">The temporary variable to be freed.</param>
+        public void FreeTemporaryVariable(string temporaryVariable)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
