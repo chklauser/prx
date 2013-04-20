0599c337742f1d2f27bad99620e81945cd3ccc85
Christian Klauser <christianklauser@outlook.com>
2013-04-20 20:16:10 +0200

    Outline namespace/builder.

--- Prexonite/Compiler/CompilerExtensions.cs
+++ Prexonite/Compiler/CompilerExtensions.cs
@@ -23,11 +23,12 @@
 //  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
 //  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
 //  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 using JetBrains.Annotations;
 using Prexonite.Compiler.Ast;
 using Prexonite.Modular;
 
-namespace Prexonite.Compiler.Internal
+namespace Prexonite.Compiler
 {
     public static class CompilerExtensions
     {
